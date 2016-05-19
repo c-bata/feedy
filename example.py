a9dc9fa0e6b3618f01ca187bbcc861183385f414
c-bata <contact@c-bata.link>
2016-05-20 05:36:10 +0900

    Update README and example

--- example.py
+++ example.py
@@ -5,24 +5,27 @@ from bs4 import BeautifulSoup
 feedy = Feedy()
 
 
-@feedy.add('http://nwpct1.hatenablog.com/rss')
+@feedy.add('http://rss.cnn.com/rss/edition.rss')
+def cnn(feed_info, entry_info, body):
+    soup = BeautifulSoup(body, "html.parser")
+    for x in soup.find_all('img'):
+        print(x['src'])
+
+
+@feedy.add('http://rss.cnn.com/rss/edition.rss')
 @social_share_plugin
-def c_bata_web(feed_info, entry_info, body, social_count):
-    """Get image urls in my blog's article"""
+def cnn_shared(feed_info, entry_info, body, social_count):
     article = {
         'url': entry_info['link'],
         'title': entry_info['title'],
-        'hatebu': social_count['hatebu_count'],
         'pocket': social_count['pocket_count'],
         'facebook': social_count['facebook_count'],
     }
-    # store article
     print(article)
 
 
 @feedy.add('https://www.djangopackages.com/feeds/packages/latest/rss/')
 def djangopackages(feed_info, entry_info, body):
-    """Get the latest django library information."""
     print("- [{pkgname}]({link})".format(pkgname=entry_info['title'], link=entry_info['link']))
 
 if __name__ == '__main__':
